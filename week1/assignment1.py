416e13c833bc45548944430d6766fbac5a37c1eb
Daniel John Idorot <ido20002@byui.edu>
2022-09-10 22:50:51 +0800

    initial commit

--- week1/assignment1.py
+++ week1/assignment1.py
@@ -8,4 +8,5 @@ place = input("Where are you from? ")
 fav_color = input("What is you favorite color? ")
 
 # Combine all the detals
-print(name + ",", "what a nice name!", place +",", "that's a wonderful place to visit!", "Your favorite color is: " + fav_color + ".")
+print(name + ",", "what a nice name!", place + ",",
+      "that's a wonderful place to visit!", "Your favorite color is: " + fav_color + ".")
