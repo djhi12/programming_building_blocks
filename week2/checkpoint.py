0d370b66077736029821b7ff521a99e2652c941c
Daniel John Idorot <ido20002@byui.edu>
2022-09-10 22:50:55 +0800

    initial commit

---
+++ week2/checkpoint.py
@@ -0,0 +1,6 @@
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+
+full_name = f"Your name is {last_name}, {first_name} {last_name}. \n"
+
+print(full_name)
