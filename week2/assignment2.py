15d505347211ee3a56bd9bb3e9e7312e9a8b134d
Daniel John Idorot <ido20002@byui.edu>
2022-09-13 22:33:49 +0800

    initial commit

--- week2/assignment2.py
+++ week2/assignment2.py
@@ -12,3 +12,30 @@ print()
 print("Your story is: \n")
 
 print(f"The other day, I was really in trouble. It all started when I saw a very {adjective} \n{animal} {verb1} down the hallway. \"{exclamation.capitalize()}!\" I yelled. But all \nI could think to do was to {verb2} over and over. Miraculously, \nthat caused it to stop, but not before it tried to {verb3} \nright in front of my family. \n")
+
+
+# Use the part of speech to create a story
+print("Part 2 story")
+adjective = input("Adjective: ")
+adjective2 = input("Adjective 2: ")
+adjective3 = input("Adjective 3: ")
+adjective4 = input("Adjective 4: ")
+noun = input("Noun: ")
+noun2 = input("Noun 2: ")
+noun3 = input("Noun 3: ")
+verb_past = input("Past tense of verb: ")
+verb_past2 = input("Past tense of ver 2: ")
+verb = input("Verb: ")
+adverb = input("Adverb: ")
+adverb2 = input("adverb 2: ")
+
+print()
+
+print("A Day At The Zoo! \n")
+print(f"Today went to the zoo. I saw a {adjective} {noun} jumping up and down in its tree. \nHe {verb_past} {adverb} through the large tunnel that led to its {adjective2} \n{noun2}. I got some peanuts and passed them through the cage to a gigantic gray {noun3} \ntowering above my head. Feeding that animal made \nme hungry. I went to get a {adjective3} scoop \nof ice cream. It filled my stomach. Afterwards I had to \n{verb} {adverb2} to catch our bus. \nWhen I got home I {verb_past2} my \nmom for a {adjective4} day at the zoo. \n")
+
+
+
+
+
+
