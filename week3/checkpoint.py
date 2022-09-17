d59e4b86ab2601f7f8c9f8c2c08a93cfcd906d6d
Daniel John Idorot <ido20002@byui.edu>
2022-09-17 18:19:46 +0800

    initial commit

---
+++ week3/checkpoint.py
@@ -0,0 +1,13 @@
+print()
+age = int(input("How old are you? "))
+egg_carton = int(input("How many eggs cartons do you have? "))
+cookies = int(input("How many cookies do you have? "))
+people = int(input("How many people are there?"))
+
+print()
+print()
+
+print(f"On your birthday, you will be {age + 1}")
+print(f"You have {egg_carton * 12} eggs")
+print(f"{cookies}")
+print(f"Each person may have {cookies / people} cookies \n")
---
+++ week3/operators_data_type.py
@@ -0,0 +1,33 @@
+number = 7
+print(f"The number is {number}. \n")
+
+color = 'blue'
+animal = 'horse' 
+
+# You can add, or concatenate, two strings together with +:
+print(color + animal)
+# This displays: bluehorse 
+
+# You can add many strings together, whether the strings are variables or directly in
+# the quotation marks:
+print(color + ' '+ animal + '!')
+# This displays: blue horse! 
+
+# You can also save the result into a new string variable: 
+combined_words = color + ' '+ animal + '!'
+print(combined_words + "\n")
+
+
+
+
+# ------
+boys_count = 10
+girls_count = 12
+
+# Add two numbers together using the + operator:
+print(boys_count + girls_count)
+# This displays: 22
+
+# You can save the result in a new variable to use later:
+total_count = boys_count + girls_count
+print(total_count + "\n")
