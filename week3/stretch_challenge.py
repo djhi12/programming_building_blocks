e4f4a4df7cb056cf4eba11262ab91a2d46639f1c
Daniel John Idorot <ido20002@byui.edu>
2022-09-17 19:37:13 +0800

    initial commit

---
+++ week3/stretch_challenge.py
@@ -0,0 +1,9 @@
+print()
+# Stretch Challenge
+import math
+
+radius = float(input("What is the radius of the circle? "))
+area = math.pi * radius ** 2
+print(f"The area of the circle is: {round(area, 2)} \n")
+
+
