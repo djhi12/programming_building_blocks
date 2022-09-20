a063565ec2b0e4e42e13e0b5adcf231e00668ac0
Daniel John Idorot <ido20002@byui.edu>
2022-09-20 19:07:15 +0800

    initial commit

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-print()
-
-# Ask the user the about the favorite color
-color = input("What is your favorite color? ")
-print(f"Your favorite color is: {color} \n")
+a = [1, 2, 3, 4, 5] 
+sum = 0
+for ele in a: sum += ele 
+print(sum)
---
+++ week4/checkpoint4.py
@@ -0,0 +1,13 @@
+print()
+
+# Fahrenheit to Celsius
+# T = 5 / 9 ( T - 32 )
+
+# Fahrenheit
+Fahrenheit = float(input("What is the temperature in Fahrenheit? "))
+
+# Convert to Celsius
+
+Celcius = 5 / 9 * (Fahrenheit - 32)
+
+print(f"The temperature in Celsius is : {Celcius:.1f} degrees. \n")
