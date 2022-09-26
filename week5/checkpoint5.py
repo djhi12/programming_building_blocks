be39cd0b649fe34c3d82ad837b0294c08aa1df09
Daniel John Idorot <ido20002@byui.edu>
2022-09-26 22:13:18 +0800

    initial commit

--- week5/checkpoint5.py
+++ week5/checkpoint5.py
@@ -1,18 +1,20 @@
+print()
+
 # Comparing number
 first_num = int(input("What is the first number? "))
 second_num = int(input("What is the second number? "))
 
 # Greater
 if first_num > second_num:
-    print("The first number is greater \n The numbers are not equal \n The second number is not greater \n")
+    print("The first number is greater \nThe numbers are not equal \nThe second number is not greater \n")
 
 # Less than
 elif first_num < second_num:
-    print("The first number is not greater \n The numbers are not equal \n The second number is greater \n")
+    print("The first number is not greater \nThe numbers are not equal \nThe second number is greater \n")
 
 # Equal
 elif first_num == second_num:
-    print("The first number is not greater \n The numbers are equal \n The second number is not greater \n")
+    print("The first number is not greater \nThe numbers are equal \nThe second number is not greater \n")
 
 # Comparing Strings
 fav_animal = input("What is your favorite animal? ")
--- week5/team_activity5.py
+++ week5/team_activity5.py
@@ -1,1 +1,39 @@
-print("hello world!")
+print()
+
+# Grade Calculator
+"""
+ A >= 90
+ B >= 80
+ C >= 70
+ D >= 60
+ F < 60
+"""
+grade_percentage = int(input("Type the grade percentage: "))
+
+sign = grade_percentage % 10
+
+if grade_percentage >= 90:
+    if sign < 3:
+        print("A-")
+    else:
+        print("A")
+elif grade_percentage >= 80:
+    if sign < 3:
+        print("B-")
+    else:
+        print("B")
+elif grade_percentage >= 70:
+    if sign < 3:
+        print("C-")
+    else:
+        print("C")
+elif grade_percentage >= 60:
+    if sign < 3:
+        print("D-")
+    else:
+        print("D")
+else:
+    print("F")
+
+
+
