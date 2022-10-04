4b8b004f3c778c2a979d3cba6ffcc14205e64050
Daniel John Idorot <ido20002@byui.edu>
2022-10-04 21:37:00 +0800

    initial commit

---
+++ week6/prove6.py
@@ -0,0 +1,65 @@
+print()
+
+# Adventure Game
+items = input("You are walking through the dark forest and find three items: a MATCH, TENT and a FLASHLIGHT. Which one do you want to pick up? ")
+
+print()
+
+# Match
+if items.lower() == "match":
+    run_walk_hide = input("You pick up the match and strike it, and for an instant, the forest around you is illuminated. You see a large grizzly bear, and then the match burns out. Do you want to RUN, WALK SLOWLY, or HIDE behind the tree? ")
+
+    print()
+
+    # Run
+    if run_walk_hide.lower() == "run":
+        print("You have to run as fast as you could so that the bear cannot catch you. \n")
+
+    # Hide
+    elif run_walk_hide.lower() == "hide":
+        print("Try not to panic; remain as quiet as possible \n")
+
+    # Walk slowly
+    elif run_walk_hide.lower() == "walk slowly":
+        print("Walk away slowly, if the bear is not approaching \n")
+
+    else:
+        print("Remain still and calm. \n")
+
+elif items.lower() == "flashlight":
+    follow_look = input("You pick up the flashlight and turn it on. You see the pathway lit up in front of you, but you thought you also heard something off to the side. Do you want to FOLLOW the path or LOOK in the trees for the thing that made the noise? ")
+
+    print()
+
+    # Follw
+    if follow_look.lower() == "follow":
+        print("Move away quietly. \n")
+
+    # Look
+    elif follow_look.lower() == "look":
+        print("If you see it, fight back with anything at hand (knife, sticks, rocks, or by kicking it). \n")
+
+    else:
+        print("Remain still and calm. \n")
+
+# Tent
+elif items.lower() == "tent":
+    tent = input("You pick up the tent. Do you  want to BRING it back to camp or SET IT UP? ")
+
+    print()
+
+    if tent.lower() == "bring":
+        print("Tell your friends that you found it in the middle of the forest. \n")
+
+    elif tent.lower() == "set it up":
+        print("Lay the base, spread out and stake your tent, add the poles, raise the tent, add the rainfly, and add final stakes and supports. \n")
+    
+    else:
+        print("Remain still and calm. \n")
+
+# Bear spray
+else:
+    print("Use a bear spray and if the bear is within ten yeards (30 feet), spray in a zigzag pattern to put a cloud of spray between yourself and the bear. \n")
+
+
+
