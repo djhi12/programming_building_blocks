37204646630f6ff40fb9ec34e847c4bc93a872de
Daniel John Idorot <ido20002@byui.edu>
2022-10-10 15:46:59 +0800

    initial commit

--- week7/guess_num.py
+++ week7/guess_num.py
@@ -4,26 +4,38 @@ print()
 from ast import Num
 import random
 
-# number = random.randint(1, 10)
-number = 10
-print(number)
+number = random.randint(1, 10)
+# number = 6
+# print(number)
 
 # Variable on numbers
 magic_num = int(input("What is the magic number? "))
 guess_num = int(input("What is your guess? "))
 
 # Loop the question
-while True:
-    if magic_num == number:
-        if guess_num <= magic_num:
-            print("Higher")
+while magic_num != number:
+    if guess_num <= number:
 
-        elif guess_num >= magic_num:
-            print("Lower")
-        
-        else:
-            print("You guessed it!")
+        print()
+
+        print("Higher")
+
+        print()
+
+        magic_num = int(input("What is the magic number? "))
+        guess_num = int(input("What is your guess? "))
+
+    elif guess_num >= magic_num:
+
+        print()
+
+        print("Lower")
     
+        print()
 
+        magic_num = int(input("What is the magic number? "))
+        guess_num = int(input("What is your guess? "))
 
+print()
 
+print("You guess it! \n")
