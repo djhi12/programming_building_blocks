66a3576f0f3beda1945b7c775b383cebb7fe7523
Daniel John Idorot <ido20002@byui.edu>
2022-10-18 15:11:08 +0800

    initial commit

--- week9/checkpoint9.py
+++ week9/checkpoint9.py
@@ -4,13 +4,18 @@ print()
 
 
 name = ""
+print()
 names = []
 while name != "end":
     name = input("Type the name of a friend: ")
-
     names.append(name)
-    # names.pop()
+
+    # Create a variable and remove the last word using [:-1]
     remove_name = names[:-1]
+
+    # Iteration
 for list_name in remove_name:
     print(list_name)
     # name = input("Type the name of a friend: ")
+
+print()
