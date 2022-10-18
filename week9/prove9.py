a31b09b0e6ed7769ebddb78b3093b59588e867de
Daniel John Idorot <ido20002@byui.edu>
2022-10-18 17:24:40 +0800

    initial commit

---
+++ week9/prove9.py
@@ -0,0 +1,56 @@
+from csv import list_dialects
+
+
+print()
+print("Welcome to the Shopping Cart Program! \n")
+
+# List
+list_items = []
+list_items_price = []
+
+action = ""
+
+while action != 5:
+    # action = int(input("Please enter an action: "))
+
+    if action == 1:
+        item = input("What item would you like to add? ")
+
+        list_items.append(item)
+        print()
+        print(f"'{item}' has been added to the cart")
+        print()
+        # action = int(input("Please enter an action: "))
+
+    elif action == 2:
+        for list_item in list_items:
+            print(list_item)
+        print()
+        # action = int(input("Please enter an action: "))
+    else:
+        print("Please enter action from 1, 2, and 5.")
+        print()
+
+    # elif action == 3:
+    #     print()
+
+    #     # action = int(input("Please enter an action: "))
+
+    # elif action == 4:
+    #     print()
+
+        # action = int(input("Please enter an action: "))
+    print("Please select one of the following: \n1. Add item \n2. View cart \n3. Remove item \n4. Compute total \n5. Quit \n")
+    action = int(input("Please enter an action: "))
+    print()
+
+print("Thank you, for shopping with us!")
+
+
+
+
+
+
+
+
+
