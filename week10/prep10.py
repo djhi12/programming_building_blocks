26e3a65bae69abdee33f0983da2d0d96ed63c99d
Daniel John Idorot <ido20002@byui.edu>
2022-10-20 08:33:44 +0800

    initial commit

---
+++ week10/prep10.py
@@ -0,0 +1,5 @@
+books = ["Nephi", "Lehi", "Alma"]
+
+for i in range(len(books)):
+    book = books[i]
+    print(f"{i}")
--- week8/prove8.py
+++ week8/prove8.py
@@ -1,65 +1,3 @@
-# print()
-
-# print("Welcome to the word guessing game!")
-# word = "wilderness"
-# print()
-
-# # print("Your hint is: ")
-# # for i, letter in enumerate(word):
-# #         print(letter, end="")
-
-# # print()
-# keep_playing = "yes"
-
-# # As long as they say "yes" we will keep playing
-# while keep_playing == "yes":
-#     print("Your hint is: ")
-#     for i, letter in enumerate(word):
-#         # print(letter, end="")
-#         hint = letter.replace(letter, "_")
-#         print(hint, end="")
-
-#     # this variable will keep track of how many guesses it took
-#     guess_count = 0
-
-#     guess = -1
-
-#     # Keep going as long as the guess doesn't match the magic number
-#     while guess != word:
-#         print()
-#         guess = input("What is your guess? ")
-#         guess_count = guess_count + 1
-
-#         for j, guess1 in enumerate(guess):
-#             print(end="")
-
-#         # if guess != word:
-#         #     print("Your guess was not correct. \n")
-#         # else:
-#         #     print("You guessed it! \n")
-
-#         if len(guess) != len(word):
-#             print("Sorry, the guess must have the same number of letters as the ")
-#         elif len(guess) == len(word) and guess != word:
-#             print("Your guess was not correct.")
-#             print("Your hint is: ")
-
-#             for i, letter in enumerate(word):
-#             # print(letter, end="")
-#                 hint = letter.replace(letter, "_")
-#                 print(hint, end="")
-                
-#         else:
-#             print("Congratulations! You guessed it!")
-
-#     # At this point, they have guessed it correctly
-#     print(f"It took you {guess_count} guesses")
-#     print()
-
-#     keep_playing = input("Would you like to play again (yes/no)? ")
-
-# print("Thank you for playing. Goodbye. \n")
-
 from winsound import PlaySound
 
 
@@ -99,13 +37,13 @@ while play_again.lower() == "yes":
             letter = guess[i]
 
             if letter == word[i]:
-                print(letter.upper())
+                print(f"{letter.upper()}", end="")
 
                 # if len(letter) != len(word[i]):
                 #     print("Sorry, the guess must have the same number of letters as the ")
 
             elif letter in word:
-                print(letter.lower())
+                print(f"{letter.lower()}", end="")
                 # print(len(letter))
 
             # elif len(guess) <= len(word):
