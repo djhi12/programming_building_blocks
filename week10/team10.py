2b4c4119ddaaa77e8f90a22bc543f3bbef58c1f0
Daniel John Idorot <ido20002@byui.edu>
2022-10-24 21:57:03 +0800

    initial commit

--- week10/team10.py
+++ week10/team10.py
@@ -6,16 +6,19 @@ bank_balances = []
 
 # Ask the user
 name = ""
-balance = "d"
+balance = 0
 print("Enter the names and balances of bank accounts (type: quit when done) \n")
-while name.lower != "quit":
 
+while name.lower() != "quit":
     # Bank Details
     name = input("What is the name of this account? ")
     bank_names.append(name)
 
     balance = float(input("What is the balance? "))
     bank_balances.append(balance)
+    print()
 
 print("Account Information:")
-
+for bank_name in bank_names:
+    bank_balance = bank_balances[bank_name]
+    print(f"{bank_name} - {bank_balance}")
