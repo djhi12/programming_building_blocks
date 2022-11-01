5c515baf8994a3fe252e756a9f7cede887b1097c
Daniel John Idorot <ido20002@byui.edu>
2022-11-01 15:26:03 +0800

    initial commit

--- week12/min_max.py
+++ week12/min_max.py
@@ -53,7 +53,7 @@ shopping_cart = [
     ["Candy bar", 0.99]
 ]
 
-max_price = -1
+max_price = 0
 max_product = "" # It doesn't matter what you set this to, it just needs to be declared
 
 for item in shopping_cart:
@@ -64,8 +64,8 @@ for item in shopping_cart:
         # This is the new max
         max_price = price
 
-# Also save this product name as the max product
-max_product = product_name
+        # Also save this product name as the max product
+        max_product = product_name
 
 print(f"The maximum price is: {max_price}")
 print(f"The product with the maximum price is: {max_product}")
