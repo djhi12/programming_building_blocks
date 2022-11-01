15fb9ab7d4a8951b6424ff3c23338995a5805bb9
Daniel John Idorot <ido20002@byui.edu>
2022-11-01 15:11:47 +0800

    initial commit

--- week11/prove11.py
+++ week11/prove11.py
@@ -2,34 +2,31 @@ print()
 # Entity, Code, Year, Life expectancy
 # Variables
 year = None
-
+i = 0
 
 with open("life-expectancy.csv") as life_expectancies:
     for life in life_expectancies:
+        i += 1
         life_details = life.strip()
-        life_details_clean = life_details.split(",")
+        life_detail_clean = life_details.split(",")
         # print(life_details_clean)
 
         # for i in range(len(life_details)):
         #     print(i)
         
         # entity
-        entity = life_details_clean[0]
-        # print(entity)
+        entity = life_detail_clean[0]
+        print(entity)
 
         # code
-        code = life_details_clean[1]
+        code = life_detail_clean[1]
 
         # year
-        year = life_details_clean[2]
+        year = life_detail_clean[2]
 
         # life expectancy
-        life_expectancy = life_details_clean[3]
-        life_expectancy_split = life_expectancy.split()
-        life_expectancy_strip = life_expectancy.strip(",")
-        # print(life_expectancy, end="")
-        print(life_expectancy_split, end="")
-        # print(life_expectancy_strip, end="")
+        life_expectancy = life_detail_clean[3]
+        
 
 
        
---
+++ week12/min_max.py
@@ -0,0 +1,18 @@
+print()
+numbers = [42, 25, 18, 83, 23, 85, 38, 2]
+largest_so_far = numbers[0]
+smallest_so_far = numbers[0]
+
+for number in numbers:
+    if number > largest_so_far:
+        # This number is larger than the largest we had seen so far
+        # So it is now the largest we've seen
+        largest_so_far = number
+
+    if number < smallest_so_far:
+        smallest_so_far = number
+
+# Now, after the loop we can display it:
+print(f"The largest is: {largest_so_far}")
+print(f"The largest is: {smallest_so_far}")
+print()
