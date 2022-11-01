1d782a8a42578ae702653ae058156f18709edf79
Daniel John Idorot <ido20002@byui.edu>
2022-11-01 15:21:53 +0800

    initial commit

--- week12/min_max.py
+++ week12/min_max.py
@@ -16,3 +16,57 @@ for number in numbers:
 print(f"The largest is: {largest_so_far}")
 print(f"The largest is: {smallest_so_far}")
 print()
+
+
+# --------------------------------------
+
+shopping_cart = [
+    ["Chips", 2.99],
+    ["Bread", 2.50],
+    ["Milk", 3.19],
+    ["Ice Cream", 6.99],
+    ["Cheese", 5.99],
+    ["Candy bar", 0.99]
+]
+
+max_price = -1
+
+for item in shopping_cart:
+    price = item[1] # The price is the second part of the item
+
+    if price > max_price:
+        # This is the new max
+        max_price = price
+
+print(f"The maximum price is: {max_price}")
+
+print()
+
+# ---------------------------------
+
+shopping_cart = [
+    ["Chips", 2.99],
+    ["Bread", 2.50],
+    ["Milk", 3.19],
+    ["Ice Cream", 6.99],
+    ["Cheese", 5.99],
+    ["Candy bar", 0.99]
+]
+
+max_price = -1
+max_product = "" # It doesn't matter what you set this to, it just needs to be declared
+
+for item in shopping_cart:
+    product_name = item[0] # Product name is the first part
+    price = item[1] 
+
+    if price > max_price:
+        # This is the new max
+        max_price = price
+
+# Also save this product name as the max product
+max_product = product_name
+
+print(f"The maximum price is: {max_price}")
+print(f"The product with the maximum price is: {max_product}")
+print()
