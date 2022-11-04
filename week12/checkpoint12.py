c7a57d149f2978226e600e82c97f31c0fc3d1266
Daniel John Idorot <ido20002@byui.edu>
2022-11-04 17:40:29 +0800

    initial commit

--- week12/checkpoint12.py
+++ week12/checkpoint12.py
@@ -10,5 +10,28 @@ people = [
     "Michael 2",
     "Jacob 10"]
 
-for i in people:
-    print(i)
+age_youngest = 5
+name_youngest = ""
+
+for name in people:
+    name_split = name.split()
+    name_age = int(name_split[1]) # Age
+    name_rank = name_split[0] # Name
+    
+    if name_age < age_youngest:
+        age_youngest = name_age
+        name_youngest = name_rank
+        
+        # print(age_youngest)
+        # print(name_youngest)
+        
+        print(f"The youngest among the names is {name_youngest} - {age_youngest}")
+    
+        
+       
+    
+    
+    
+    
+    
+   
