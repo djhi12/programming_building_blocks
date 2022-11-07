827166158cb627fe73d5db2a8dacb10e94c0c3c1
Daniel John Idorot <ido20002@byui.edu>
2022-11-07 13:33:30 +0800

    initial commit

--- week12/team12.py
+++ week12/team12.py
@@ -1,16 +1,27 @@
 print()
 
+# Largest number
+scripture_largest_num = 0
+scripture_largest_num_book = ""
+
 with open("scriptures12.txt") as scriptures:
     for scripture in scriptures:
         scripture_strip = scripture.strip() # Remove extra spaces
         scripture_split = scripture_strip.split(":") # Separate items
+        # print(scripture_split)
+     
+        # Display format
+        # print(f"Scripture: {scripture_split[2]}, Book: {scripture_split[0]}, Chapters: {scripture_split[1]}")
         
-        # Access index
-        scriptures_name = scripture_split[2] 
-        books = scripture_split[0]
-        books_chapters = scripture_split[1]
-        
-        
+        # Largest number
+        if int(scripture_split[1]) > scripture_largest_num:
+            scripture_largest_num = int(scripture_split[1])
+            scripture_largest_num_book = scripture_split[0]
+            
+            if scripture_largest_num == 150:
+                print(f"The {scripture_largest_num_book} has the largest scripture verse {scripture_largest_num}.")
+    
+    
         
         
         
