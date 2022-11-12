c0e57111a8dc7ce295f05c5ccd4df3d5eba410e9
Daniel John Idorot <ido20002@byui.edu>
2022-11-12 20:34:49 +0800

    initial commit

--- week12/prove12.py
+++ week12/prove12.py
@@ -1,4 +1,5 @@
 print()
+from statistics import mean
 # Entity, Code, Year, Life expectancy
 
 inquire = "yes"
@@ -10,44 +11,42 @@ while inquire == "yes":
     i = 0
     with open("../week11/life-expectancy.csv") as life_expectancies:
         i += 1
+        
         country_name = ""
         country_largest = 0
+        country_lowest = 1000
+        country_average = 0
         country = input("Type the country: ")
-        
+        country_expectancy = input("Type LARGEST OR LOWEST for life expectancy: ")
+        print()
         
         for details in life_expectancies:
             # strip and split
             details_strip = details.strip()
-            details_split = details_strip.split(",")
+            details_split = details_strip.split(",") # split
             
-        
-            if country.title() == details_split[0]:
+            # Highest
+            if country.title() == details_split[0] and country_expectancy.lower() == "largest":
                 
-                # for i in range(len(details_split)):
-                #     print(f"Country: {details_split[0]} - Code: {details_split[1]} - Year: {details_split[2]} - Life Expectancy: {float(details_split[3])}")
-                    
-                if float(details_split[3]) > country_largest:
-                    country_largest = float(details_split[3])
-                    country_name = details_split[0]
-                    
-                    print(f"{country_name} - {country_largest}")
-                    
-                    
-        else:
-            print("Type the proper name of the country.")
-               
-        
-        # 
-    
-    
-    
-    
-    
+                for i in range(len(details_split)):
+                    if float(details_split[3]) > country_largest:
+                        country_largest = float(details_split[3])
+                        print(f"Largest years of expectancy: {details_split[2]} - {country_largest}")
+            
+            # Lowest
+            elif country.title() == details_split[0] and country_expectancy.lower() == "lowest":
+                
+                for i in range(len(details_split)):
+                    if float(details_split[3]) < country_lowest:
+                        country_lowest = float(details_split[3])
+                        print(f"Lowest years of expectancy: {details_split[2]} - {country_lowest}")                
+            
     
     inquire = input("Do you want to inquire again (yes/no)?")
+    print()
     
     
-print("Thank you, for inquiring!")
+print("Thank you, for inquiring! \n")
         
         
             
