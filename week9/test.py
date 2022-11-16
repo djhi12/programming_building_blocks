60fdcad874983040024ca65570525f1c67367cca
Daniel John Idorot <ido20002@byui.edu>
2022-11-16 15:25:29 +0800

    initial commit

--- week9/test.py
+++ week9/test.py
@@ -1,63 +1,35 @@
-from csv import list_dialects
-import math
-
 print()
-print("Welcome to the Shopping Cart Program! \n")
+
+print("Welcome to the shopping cart program!")
 
 # List
 list_items = []
 list_items_price = []
 
-action = 0
-price = 0
-item_remove = ""
+action = ""
 
 while action != 5:
-
-    # Add item and price
+    
     if action == 1:
-        item = input("What item would you like to add? ")
-        price = float(input(f"What is the price of '{item}'? "))
-
-        list_items.append(item)
-        list_items_price.append(price)
-       
-        print()
-        print(f"'{item}' has been added to the cart")
-        print()
+        item = input("What item would like to add? ")
         
-    # Display items
+        list_items.append(item) # Add item
+        print(f"'{item}' has been added to the cart.")
+        
     elif action == 2:
         print("The contents of the shopping cart are:")
-    
-        for i in range(len(list_items)):
-            print(f"{i + 1}. {list_items[i]} - {list_items_price[i]:.2f}")
+        
+        for list_item in list_items:
+            print(list_item)
             
-       
-
-        # Number of items
-        print(f"You have {len(list_items)} items in your cart. \n")
-
-    # Remove item
     elif action == 3:
-       print()
-
-    # Total items
-    elif action == 4:
-      
         print()
-
-    print("Please select one of the following: \n1. Add item \n2. View cart \n3. Remove item \n4. Compute total \n5. Quit \n")
+        
+        
+    elif action == 4:
+        print()
+    
     action = int(input("Please enter an action: "))
-    print()
-
-print("Thank you, for shopping with us! \n")
-
-
-
-
-
-
-
-
+    
+print("Thank you shopping with us!")
 
