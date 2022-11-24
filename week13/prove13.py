2b06c0d5ee71c5437819a3714972d3cd68d20bae
Daniel John Idorot <ido20002@byui.edu>
2022-11-24 10:36:41 +0800

    initial commit

--- week13/prove13.py
+++ week13/prove13.py
@@ -2,5 +2,50 @@ print()
 # Wind Chill Calculations 
 
 # Wind Chill
-def wind_chill():
-    print()
+# Fahrenheit to Celcius = C = 5/9 (F - 32)
+# Celcius to Fahrenheit = F = C (9 / 5) + 32
+
+# Wind Chill
+def wind_chill(temp, wind_speed=0):
+    return (35.74 + 0.6215 * temp) - (35.75 * wind_speed ** 0.16) + (0.4275 * wind_speed ** 0.16)
+
+      
+# Fahrenheit
+def fahrenheit(temp):
+    return temp * (9 / 5) + 32
+
+# Celcius
+def celcius(temp):
+    return 5/ 9 * (temp - 32)
+
+
+def temp_compute(temp_type, value1):
+    temperature = -1
+    
+    if temp_type == "F":
+        temperature = fahrenheit(value1)
+    elif temp_type == "C":
+        temperature = celcius(value1)
+        
+    return temperature
+
+
+# Main program
+temp_type = ""
+
+while temp_type != "":
+    temp_num = float(input("What is the temperature? "))
+    temp_type = input("Fahrenheit or Celcius (F/C)? ")
+    
+    temp_type.upper()
+    
+    if temp_type == "F":
+        temperature = temp_compute(temp_type, temp_num)
+        print(temperature)
+    elif temp_type == "C":
+        temperature = temp_compute(temp_type, temp_num)
+        print(temperature)
+        
+    
+
+    
