1f5c73e37e0224de4d7bd1ee75f52a3cda2e9f90
Daniel John Idorot <ido20002@byui.edu>
2022-11-25 14:34:04 +0800

    initial commit

--- week12/prove.py
+++ week12/prove.py
@@ -1,62 +1,133 @@
 print()
-i = 0
-with open("../week11/life-expectancy.csv") as life_expectancies:
-    year_of_interest = int(input("Enter the year of interest: "))
+# from statistics import mean
+# # Entity, Code, Year, Life expectancy
+
+# inquire = "yes"
+
+# while inquire == "yes":
+#     inquire = ""
     
-    max_life_all = ""
-    min_life_all = ""
-    max_life_all_exp = 0
-    min_life_all_exp = 1000
     
-    ave_life_exp = 0
+#     i = 0
+#     with open("../week11/life-expectancy.csv") as life_expectancies:
+#         i += 1
+        
+#         country_name = ""
+#         country_largest = 0
+#         country_lowest = 1000
+#         country_average = 0
+#         country = input("Type the country: ")
+#         country_expectancy = input("Type LARGEST OR LOWEST for life expectancy: ")
+#         print()
+        
+#         for details in life_expectancies:
+#             # strip and split
+#             details_strip = details.strip()
+#             details_split = details_strip.split(",") # split              
+            
     
-    max_life_country = ""
-    min_life_country = ""
-    max_life_exp = 0
-    min_life_exp = 1000
-    
-    for details in life_expectancies:
+inquire = "yes"
+
+while inquire == "yes":
+    inquire = ""
+    with open("../week11/life-expectancy.csv") as life_expectancies:
+        country_name = ""
+        country_largest = 0
+        country_lowest = 1000
+        country_average = 0
+        
+        
+        print()
+        i = 0
+        max_life_expectancy = 0
+        min_life_expectancy = 1000
+        average_life_ecpectancy = 0
+        year_of_interest = int(input("Enter the year of interest: "))
+        country = input("Type the country: ")
+        country_expectancy = input("Type LARGEST OR LOWEST for life expectancy: ")
+        
+        for details in life_expectancies:
             i += 1
             # strip and split
             details_strip = details.strip()
             details_split = details_strip.split(",") # split
+            # details_split_float = float(details_split[3])
             
             if i > 1:
-                year = int(details_split[2])
-                life_exp = float(details_split[3])
-                country = details_split[0]
-                code = details_split[1]
+                details_split_float = float(details_split[3])
+                details_split_year = int(details_split[2])
+                # print(details_split_year)
                 
-                if life_exp > max_life_all_exp:
-                    max_life_all_exp = life_exp
-                    max_life_all = country
+                if details_split_year == year_of_interest:
                     
-                if life_exp < min_life_all_exp:
-                    min_life_all_exp = life_exp
-                    min_life_all = country
-                    
+                    # print(max(details_split_float))
+                    if details_split_float > max_life_expectancy:
+                        max_life_expectancy = details_split_float
                 
-                if year_of_interest == year:
-                     
-                    
-                    
-                    if life_exp > max_life_exp:
-                        max_life_exp = life_exp
-                        max_life_country = country
                         
-                        # print(f"{max_life_all} - {max_life_exp}")
+                    elif details_split_float < min_life_expectancy:
+                        min_life_expectancy = details_split_float
+                        # average_life_ecpectancy = average_life_ecpectancy + details_split_float
+                        # average_life_ecpectancy = average_life_ecpectancy / details_split_year
+                        
+            # Highest
+            if country.title() == details_split[0] and country_expectancy.lower() == "largest":
+                
+                for i in range(len(details_split)):
+                    if float(details_split[3]) > country_largest:
+                        country_largest = float(details_split[3])
+                        
+            
+            # Lowest
+            elif country.title() == details_split[0] and country_expectancy.lower() == "lowest":
+                
+                for i in range(len(details_split)):
+                    if float(details_split[3]) < country_lowest:
+                        country_lowest = float(details_split[3])
+                        
+                                            
+        
+        print(f"The overall max life expectancy is: {max_life_expectancy} from {details_split[0]} in {details_split_year}")        
+        
+        print(f"The overall min life expectancy is: {min_life_expectancy} from {details_split[0]} in {details_split_year} \n")
+
+        print(f"For the year {year_of_interest}:")
+        print(f"The average life expectancy across all countries was {average_life_ecpectancy}")
+        print(f"The max life expectancy was in {details_split[0]} with {max_life_expectancy}")
+        print(f"The min life expectancy was in {details_split[0]} with {min_life_expectancy} \n")
+        print(f"Largest years of expectancy: {details_split[2]} - {country_largest}")
+        print(f"Lowest years of expectancy: {details_split[2]} - {country_lowest}")
+    
+    inquire = input("Do you want to inquire again (yes/no)?")
+    print()
+    
+    
+print("Thank you, for inquiring! \n")
+                        
+                        
+            
                     
-                    
-                    if life_exp < min_life_exp:
-                        min_life_exp = life_exp
-                        min_life_country = country
-                 
+            
+            
+           
+            
+       
+             
+             
+    
 
 
-print(f"The overall max life expectancy is: {max_life_all_exp} from {max_life_all} in {year}")
-print(f"The overall min life expectancy is: {min_life_all_exp} from {min_life_all} in {year} \n")
 
+        
+        
+            
+            
+            
+        
+        
+    
+                
+                
+                
 
-print(f"For the year {year_of_interest}:")
-print(f"The max life expectancy was in {max_life_country} with {max_life_exp}")
-print(f"The min life expectancy was in {min_life_country} with {min_life_exp}")
+          
